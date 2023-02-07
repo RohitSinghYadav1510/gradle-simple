703616bef92272ef42a7ea7173fb166f19cd60ec
Rohit Singh Yadav <47585976+RohitSinghYadav1510@users.noreply.github.com>
2023-02-07 14:16:10 +0530

    Update Hello.java

--- src/main/java/Hello.java
+++ src/main/java/Hello.java
@@ -1,7 +1,7 @@
 
 public class Hello {
   
-  public static String GREETING = "Hello world! ";
+  public static String GREETING = "Hello world! hello duniya";
   
   public static void main(String []args) {
     
